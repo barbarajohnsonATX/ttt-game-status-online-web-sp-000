bcd59146f6e0f08a7c9f4e9350a0301cefa39f09
Barbara Johnson <johnson.barbara@gmail.com>
2019-03-31 20:58:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,10 +19,10 @@ def won?(board)
 
 WIN_COMBINATIONS.each do |combo| 
 if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
-  puts [combo[0], combo[1], combo[2]]
+   [combo[0], combo[1], combo[2]]
 
 elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
-  puts [combo[0], combo[1], combo[2]]
+   [combo[0], combo[1], combo[2]]
   
 else
   FALSE
